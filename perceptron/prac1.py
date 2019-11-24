7e03f2a826764f87b2f0b06a9cc6001a024c65e9
tomoya yanagibashi <t.bashiyy@gmail.com>
2019-11-24 16:07:40 +0900

    update

---
+++ perceptron/prac1.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def AND(x1, x2):
+  x = np.array([x1,x2])
+  w = np.array([0.5,0.5])
+  b = -0.7
+  tmp = sum(x*w) + b
+  if tmp <= 0:
+    return 0
+  elif tmp > 0:
+    return 1
+
+def NAND(x1,x2):
+  x = np.array([x1,x2])
+  w = np.array([-0.5,-0.5])
+  b = 0.7
+  tmp = sum(x*w) + b
+  if tmp <= 0:
+    return 0
+  elif tmp > 0:
+    return 1
+
+def OR(x1,x2):
+  x = np.array([x1,x2])
+  w = np.array([0.5,0.5])
+  b = -0.2
+  tmp = sum(x*w) + b
+  if tmp <= 0:
+    return 0
+  elif tmp > 0:
+    return 1
+
