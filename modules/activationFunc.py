4781c5edd1c59091bc07bd2220cc39ecb570d240
tomoya yanagibashi <t.bashiyy@gmail.com>
2019-12-09 23:42:54 +0900

    update

--- modules/activationFunc.py
+++ modules/activationFunc.py
@@ -5,3 +5,9 @@ def step_function(x):
 
 def sigmoid(x):
   return 1 / (1 + np.exp(-x))
+
+def softmax(x):
+  c = np.max(x)
+  exp_a = np.exp(x-c)
+  sum_exp_a = np.sum(exp_a)
+  return exp_a/sum_exp_a
